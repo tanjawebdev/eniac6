eea40e3e62fce7606ed34902129f01d93de3458d
Tanja Kobler <kobler.tanja@gmail.com>
2026-09-09 12:20:49 +0200

    changed quotes

--- client/src/components/QuoteBlock/QuoteBlock.tsx
+++ client/src/components/QuoteBlock/QuoteBlock.tsx
@@ -50,6 +50,7 @@ export function QuoteBlock() {
             </span>
           ))}
         </p>
+        <span className="quote-block-speaker">- {quoteEntry.speaker}</span>
       </div>
 
       {/* Context section */}
